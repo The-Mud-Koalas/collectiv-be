c8ca7b44f2ece912a8e462b8769fad30e240df6d
johanes.christian <johanes.christian@ui.ac.id>
2023-08-25 00:26:31 +1000

    chores: Remove insecure key from settings.py

--- communalspace/settings.py
+++ communalspace/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-kdp$qlc=09)l)ze38+e08p@)_@13f09$9*hr@#@fhvz@%2$ao#')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
