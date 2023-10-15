ab32bc869e2c592a39d62accfcdb3750855c05e3
johanes.christian <johanes.christian@ui.ac.id>
2023-10-15 21:56:14 +1000

    feat: Implement get location by ID

--- space/urls.py
+++ space/urls.py
@@ -4,11 +4,13 @@ from .views import (
     serve_get_or_create_location,
     serve_get_nearby_non_subscribed_locations,
     serve_get_list_of_subscribed_locations,
-    serve_subscribe_or_neglect_location
+    serve_subscribe_or_neglect_location,
+    serve_get_location_by_id,
 )
 
 urlpatterns = [
     path('all/', serve_get_all_locations),
+    path('<str:location_id>/', serve_get_location_by_id),
     path('get-or-create/', serve_get_or_create_location),
     path('nearby/nonsubscribed/', serve_get_nearby_non_subscribed_locations),
     path('preference/update/', serve_subscribe_or_neglect_location),
