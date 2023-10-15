20852d1ca70adf7d6b5549a73775122a310e3203
Rashad Aziz <rashad.aziz@ui.ac.id>
2023-10-15 19:32:54 +1000

    fix: made limit parameter optional

--- forums/services/range_find_post.py
+++ forums/services/range_find_post.py
@@ -22,7 +22,7 @@ def find_post_in_range(request_data, user, event_id):
         raise RestrictedAccessException('user is not part of event with id ' + event_id)
 
     request_limit = request_data.get("limit")
-    limit = int(request_limit) if request_limit is not None else 10
+    limit = int(request_limit) if request_limit is not None else None
     request_after = request_data.get("after")
     after = app_utils.get_date_from_date_time_string(request_after) if request_after is not None else None
     request_before = request_data.get("before")
@@ -36,6 +36,7 @@ def find_post_in_range(request_data, user, event_id):
         posts = posts.filter(posted_at__lt=before)
     if after:
         posts = posts.filter(posted_at__gt=after)
-    posts = posts.order_by("-posted_at")[:limit]
+    if limit:
+        posts = posts.order_by("-posted_at")[:limit]
 
     return posts
